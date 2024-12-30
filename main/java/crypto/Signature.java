275b88d63d8e73f6db70c3639960f8e484b67891
Yaroslav9901 <erolio1267@gmail.com>
2024-12-30 08:15:25 +0300

    Update Signature.java

--- main/java/crypto/Signature.java
+++ main/java/crypto/Signature.java
@@ -1,7 +1,9 @@
 package crypto;
 
 import java.security.PublicKey;
-
+/**
+ * Класс Signature представляет собой реализацию интерфейса цифровой подписи.
+ */
 public interface Signature {
     String sign(String data) throws Exception;
     boolean verify(String data, String signatureStr, PublicKey publicKey) throws Exception;
