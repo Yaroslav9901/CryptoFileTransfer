e461ae0a52146722b4d003e651784ff413cf134b
Yaroslav9901 <erolio1267@gmail.com>
2024-12-30 08:14:38 +0300

    Update Encryptor.java

--- main/java/crypto/Encryptor.java
+++ main/java/crypto/Encryptor.java
@@ -1,5 +1,7 @@
 package crypto;
-
+/**
+ * Класс Encryptor представляет собой реализацию шифровальщика.
+ */
 public interface Encryptor {
     String encrypt(byte[] data) throws Exception;
     String decrypt(String encryptedData) throws Exception;
