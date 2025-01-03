c21dbb4fe662285199050e8993f4b1a00c5676a9
Yaroslav9901 <erolio1267@gmail.com>
2025-01-04 02:54:24 +0300

    Delete main directory

--- main/java/crypto/Encryptor.java
+++
@@ -1,9 +0,0 @@
-package crypto;
-/**
- * Класс Encryptor представляет собой реализацию шифровальщика.
- */
-public interface Encryptor {
-    String encrypt(byte[] data) throws Exception;
-    String decrypt(String encryptedData) throws Exception;
-    String getSecretKey();
-}
